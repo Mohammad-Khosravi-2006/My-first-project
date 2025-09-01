492d826bf1d4ce241592c8ddca0b57bc23643f48
Mohammad Khosravi <mohammad.khosravi.8506@gmail.com>
2025-09-01 16:52:15 +0330

    Without Any Freamwork

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function myfunc(){
+    var x = document.getElementById("mynav");
+    if(x.className === "nav"){
+        x.className += " responsive";
+    }
+    else{
+        x.className = "nav";
+    }
+}
